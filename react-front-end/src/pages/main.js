dc235189bcebed297494cc8252d85985094d7c45
Timi Alabi <alabitimi65@gmail.com>
2022-08-08 21:53:12 +0000

    added popular bookclubs section

--- react-front-end/src/pages/main.js
+++ react-front-end/src/pages/main.js
@@ -3,62 +3,40 @@ import "./main.scss";
 
 const MainPage = () => {
   return (
-    <section className="content">
-      <div className="hero-section">
-        <div className="hero-header-box">
-          <h1 className="hero-header"> A Book Lovers Hub</h1>
-        </div>
+    <section className="main-contianer">
+      <div className="content">
+        <div className="hero-section">
+          <div className="hero-header-box">
+            <h1 className="hero-header"> A Book Lovers Hub</h1>
+          </div>
 
-        <div className="hero-text-box">
-          <span className="hero-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            vulputate massa et posuere dictum. Pellentesque habitant morbi
-            tristique senectus et netus et malesuada fames ac turpis egestas
-          </span>
+          <div className="hero-text-box">
+            <span className="hero-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              vulputate massa et posuere dictum. Pellentesque habitant morbi
+              tristique senectus et netus et malesuada fames ac turpis egestas
+            </span>
+          </div>
+          <div className="cta-box">
+            <button className="cta-button">Start a Bookclub</button>
+          </div>
         </div>
-        <div className="cta-box">
-          <button className="cta-button">Start a Bookclub</button>
+        <div className="hero-image-box">
+          <div className="blob"></div>
         </div>
       </div>
-      <div className="hero-image-box">
-        <div className="blob"></div>
+
+      <div className="popular-bookclubs-section">
+        <h2 className="popular-bookclubs-header">Popular Bookclubs</h2>
+        <div className="popular-bookclubs-container">
+          <div className="popular-bookclubs-card">First Card</div>
+          <div className="popular-bookclubs-card">Second Card</div>
+          <div className="popular-bookclubs-card">Third Card</div>
+          <div className="popular-bookclubs-card">Fourth Card</div>
+          <div className="popular-bookclubs-card">Fifth Card</div>
+          <div className="popular-bookclubs-card">Last card</div>
+        </div>
       </div>
-
-      {/* <section className="recommended-books-section">
-        <div className="recommended-headerbox">
-          <h2 className="recommended-books-header">Recommended Books</h2>
-        </div>
-
-        <div className="recommended-books">
-          <div className="recommended-books-card">
-            <div className="card-details">
-              <span className="card-title">Avengers: End Game</span>
-              <span className="author">Salma Ibrahim</span>
-            </div>
-          </div>
-
-          <div className="recommended-books-card">
-            <div className="card-details">
-              <span className="card-title">Avengers: End Game</span>
-              <span className="author">Salma Ibrahim</span>
-            </div>
-          </div>
-
-          <div className="recommended-books-card">
-            <div className="card-details">
-              <span className="card-title">Avengers: End Game</span>
-              <span className="author">Salma Ibrahim</span>
-            </div>
-          </div>
-
-          <div className="recommended-books-card">
-            <div className="card-details">
-              <span className="card-title">Avengers: End Game</span>
-              <span className="author">Salma Ibrahim</span>
-            </div>
-          </div>
-        </div>
-      </section> */}
     </section>
   );
 };
