4df80db308ea1fa3b8e3324a2eecca1fd9761f5e
Timi Alabi <alabitimi65@gmail.com>
2022-08-08 17:14:17 +0000

    removed recommended books

--- react-front-end/src/pages/login.js
+++ react-front-end/src/pages/login.js
@@ -1,10 +1,27 @@
 import React from "react";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>
-      <h2>This is the Login Page!</h2>
-    </div>
+    <section className="login-section">
+      <div className="login-header-box">
+        <h1 className="login-header">Login</h1>
+      </div>
+
+      <div className="form-box">
+        <form>
+          <label className="username"></label>
+          <placeholder>Username</placeholder>
+        </form>
+
+        <form>
+          <label className="username"></label>
+          <placeholder>Passowrd</placeholder>
+        </form>
+
+        <button className="login-btn">Login</button>
+      </div>
+    </section>
   );
 };
 
