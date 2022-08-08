f5e53f33729e9c25ff247151e82639f00ddafdb6
Timi Alabi <alabitimi65@gmail.com>
2022-08-08 15:34:14 +0000

    integrated db to the backend folder

--- react-front-end/src/pages/signup.js
+++ react-front-end/src/pages/signup.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "./signup.scss";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>This is the SignUp Page!</h2>
-    </div>
+    <section className="sign-up-section">
+      <div className="sign-up-header-box">
+        <h1 className="sign-up-header"> Sign Up</h1>
+      </div>
+
+      <div className="form-box">
+        <form>
+          <label className="username"></label>
+          <placeholder>Username</placeholder>
+        </form>
+
+        <form>
+          <label className="username"></label>
+          <placeholder>Email</placeholder>
+        </form>
+
+        <form>
+          <label className="username"></label>
+          <placeholder>Password</placeholder>
+        </form>
+
+        <button className="signup-btn"> Sign Up</button>
+      </div>
+    </section>
   );
 };
 
